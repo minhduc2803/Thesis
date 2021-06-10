d4f7c76ada278ad713b12209a4371b1c3d1f879b
minhduc2803 <hontomoni@gmail.com>
2021-06-10 18:27:11 +0700

    WIP: add SCPO decode

--- src/main/java/linearize/SCPO.java
+++ src/main/java/linearize/SCPO.java
@@ -40,7 +40,7 @@ public class SCPO implements Curve {
       for (NearestArray.Node pickedNode : pickedNodes) {
         int x = pickedNode.x;
         int y = pickedNode.y;
-        if (x >=0 && x < width && y >=0 && y < height && markedNode[x][y] == 0) {
+        if (x >= 0 && x < width && y >= 0 && y < height && markedNode[x][y] == 0) {
           int value = origin[x][y];
           frontier.push(pickedNode, value);
           markedNode[x][y] = 1;
@@ -53,15 +53,54 @@ public class SCPO implements Curve {
 
   @Override
   public int[][] decode(int[] encodeData) {
-    return new int[0][];
+    int n = encodeData.length;
+    int width = (int) Math.sqrt(n);
+    int height = width;
+    int[][] result = new int[width][height];
+    int[][] markedNode = new int[width][height];
+
+    NearestArray.Node node = new NearestArray.Node(0, 0);
+    int index = 0;
+    int lastValue = encodeData[index++];
+    frontier.push(node, lastValue);
+    markedNode[node.x][node.y] = 1;
+    result[node.x][node.y] = lastValue;
+
+    while (index < n) {
+      node = frontier.pop(lastValue);
+      lastValue = result[node.x][node.y];
+
+      NearestArray.Node[] pickedNodes = {
+          new NearestArray.Node(node.x + 1, node.y),
+          new NearestArray.Node(node.x, node.y + 1),
+          new NearestArray.Node(node.x - 1, node.y),
+          new NearestArray.Node(node.x, node.y - 1)
+      };
+
+      for (NearestArray.Node pickedNode : pickedNodes) {
+        int x = pickedNode.x;
+        int y = pickedNode.y;
+        if (x >= 0 && x < width && y >= 0 && y < height && markedNode[x][y] == 0) {
+          int value = encodeData[index++];
+          frontier.push(pickedNode, value);
+          markedNode[x][y] = 1;
+
+          result[x][y] = value;
+        }
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
     SCPO scpo = new SCPO();
     int[][] origin = {{1, 7, 3}, {4, 6, 8}, {5, 2, 9}};
-    int[] result = scpo.encode(origin);
+    int[] encodeData = scpo.encode(origin);
+    int[][] decodeData = scpo.decode(encodeData);
 
     Print.print2DArray("Origin data", origin);
-    Print.print1DArray("Encode data", result);
+    Print.print1DArray("Encode data", encodeData);
+    Print.print2DArray("Decode data", decodeData);
   }
 }
